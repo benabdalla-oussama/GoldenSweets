b230c48e6de5e1ec3444f902f44e084a61d8f3a8
oussama <oussabdalla@gmail.com>
2018-12-07 01:31:35 +0100

    First Commit of GoldenSweets Project that containes Authentication, Cakes,  Orders, GalleryImagesand ShoppingCart Models

---
+++ GoldenSweets/Migrations/20181205230848_add_GalleryId_in_GalleryImage_Model.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GoldenSweets.Migrations
+{
+    public partial class add_GalleryId_in_GalleryImage_Model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+            migrationBuilder.CreateTable(
+                name: "Galleries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GalleryTime = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(maxLength: 255, nullable: false),
+                    Selected = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Galleries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GalleryImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    GalleryId = table.Column<int>(nullable: true),
+                    Label = table.Column<string>(maxLength: 255, nullable: false),
+                    Url = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GalleryImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GalleryImage_Galleries_GalleryId",
+                        column: x => x.GalleryId,
+                        principalTable: "Galleries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GalleryImage_GalleryId",
+                table: "GalleryImage",
+                column: "GalleryId");
+
+          
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+         
+
+            migrationBuilder.DropTable(
+                name: "GalleryImage");
+
+            migrationBuilder.DropTable(
+                name: "Galleries");
+
+          
+        }
+    }
+}
---
+++ GoldenSweets/Migrations/20181206154912_add_SelectedAttribute_to_GalleryImage_Model.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GoldenSweets.Migrations
+{
+    public partial class add_SelectedAttribute_to_GalleryImage_Model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GalleryImage_Galleries_GalleryId",
+                table: "GalleryImage");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetail_Orders_OrderId",
+                table: "OrderDetail");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderDetail",
+                table: "OrderDetail");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GalleryImage",
+                table: "GalleryImage");
+
+            migrationBuilder.RenameTable(
+                name: "OrderDetail",
+                newName: "OrderDetails");
+
+            migrationBuilder.RenameTable(
+                name: "GalleryImage",
+                newName: "GalleryImages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderDetail_OrderId",
+                table: "OrderDetails",
+                newName: "IX_OrderDetails_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_GalleryImage_GalleryId",
+                table: "GalleryImages",
+                newName: "IX_GalleryImages_GalleryId");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Selected",
+                table: "GalleryImages",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderDetails",
+                table: "OrderDetails",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GalleryImages",
+                table: "GalleryImages",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GalleryImages_Galleries_GalleryId",
+                table: "GalleryImages",
+                column: "GalleryId",
+                principalTable: "Galleries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Orders_OrderId",
+                table: "OrderDetails",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GalleryImages_Galleries_GalleryId",
+                table: "GalleryImages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Orders_OrderId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderDetails",
+                table: "OrderDetails");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GalleryImages",
+                table: "GalleryImages");
+
+            migrationBuilder.DropColumn(
+                name: "Selected",
+                table: "GalleryImages");
+
+            migrationBuilder.RenameTable(
+                name: "OrderDetails",
+                newName: "OrderDetail");
+
+            migrationBuilder.RenameTable(
+                name: "GalleryImages",
+                newName: "GalleryImage");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderDetails_OrderId",
+                table: "OrderDetail",
+                newName: "IX_OrderDetail_OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_GalleryImages_GalleryId",
+                table: "GalleryImage",
+                newName: "IX_GalleryImage_GalleryId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderDetail",
+                table: "OrderDetail",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GalleryImage",
+                table: "GalleryImage",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GalleryImage_Galleries_GalleryId",
+                table: "GalleryImage",
+                column: "GalleryId",
+                principalTable: "Galleries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetail_Orders_OrderId",
+                table: "OrderDetail",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
